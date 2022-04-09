93a92ff7c5923657b134f4f173c8d0e1e9532615
PAPAYA <dameningen@126.com>
2022-04-08 21:19:21 -0400

    save point

--- Assets/CombatFolder/Scripts/SavePointManager.cs
+++ Assets/CombatFolder/Scripts/SavePointManager.cs
@@ -1,21 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SavePointManager : MonoBehaviour
 {
 	static public SavePointManager me;
 	private GameObject _player;
 	private PlayerScriptNew _ps;
+	private List<GameObject> _allBears;
+	private List<GameObject> _activatedBears;
 	public SafeHouseTrigger last_checkPoint;
+
+	private void Awake()
+	{
+		me = this;
+	}
 
 	private void Start()
 	{
 		_player = PlayerScriptNew.me.gameObject;
 		_ps = PlayerScriptNew.me;
-		me = this;
+		_allBears = GameObject.FindGameObjectsWithTag("Enemy").ToList();
+		_activatedBears = new List<GameObject>();
 	}
 
 	public void ResetPlayer()
 	{
+		if (last_checkPoint == null)
+		{
+			Debug.LogError("You haven't got to a check point!");
+		}
 		if (last_checkPoint.rebornPos != null)
 		{
 			// set player position
@@ -32,6 +47,64 @@ public class SavePointManager : MonoBehaviour
 		else
 		{
 			Debug.LogError("Assign reborn position to safe house!");
+		}
+	}
+
+	public void ResetBears()
+	{
+		GetActivatedBears();
+		foreach (var bear in _activatedBears)
+		{
+			bear.transform.position = bear.GetComponent<Enemy>().ogTransform.position;
+			bear.transform.rotation = bear.GetComponent<Enemy>().ogTransform.rotation;
+			bear.GetComponent<AIController>().enabled = false;
+			foreach (var door in bear.GetComponent<Enemy>().myEntrances)
+			{
+				door.CloseFront();
+				if (door.GetComponent<DialogueScript>() != null)
+				{
+					door.GetComponent<DialogueScript>().enabled = false;
+				}
+				door.myOpenTrigger.SetActive(true);
+				door.bearsBehind.Add(bear);
+			}
+		}
+		
+	}
+
+	private void GetActivatedBears()
+	{
+		_activatedBears.Clear();
+		// get bears that are activated
+		foreach (var bear in _allBears.ToList())
+		{
+			if (bear != null)
+			{
+				if (bear.GetComponent<SmallBear>()) // small bear
+				{
+					if (bear.GetComponent<AIController>().enabled &&
+						bear.GetComponent<SmallBear>().health > 0)
+					{
+						
+						if (!_activatedBears.Contains(bear))
+						{
+							_activatedBears.Add(bear);
+						}
+					}
+				}
+				else // big bear
+				{
+					if (bear.GetComponent<Enemy>() != null &&
+						!bear.GetComponent<Enemy>().phase.Equals(Enemy.AIPhase.NotInBattle) &&
+						bear.GetComponent<Enemy>().health > 0)
+					{
+						if (!_activatedBears.Contains(bear))
+						{
+							_activatedBears.Add(bear);
+						}
+					}
+				}
+			}
 		}
 	}
 }
