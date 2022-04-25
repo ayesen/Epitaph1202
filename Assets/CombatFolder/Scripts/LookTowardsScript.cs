46569accbd8e91f38adb77075acaf78f23c0d554
PAPAYA <dameningen@126.com>
2022-04-25 14:47:03 -0400

    looktowards door or anything

---
+++ Assets/CombatFolder/Scripts/LookTowardsScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTowardsScript : MonoBehaviour
+{
+    public GameObject lookTarget;
+    private PlayerScriptNew ps;
+
+    private void Start()
+    {
+        ps = PlayerScriptNew.me;
+    }
+
+    public void LookTowards()
+    {
+        if (lookTarget != null)
+        {
+            StartCoroutine(ps.LookTowardsItem(lookTarget));
+        }
+    }
+}
